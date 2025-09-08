e915abd6a6cc868623046976ced34897ae1585b9
Udoy Kumar <udoypal.kumar2002@gmail.com>
2025-09-08 18:10:40 +0600

    full code recheck simple change

--- script.js
+++ script.js
@@ -77,7 +77,10 @@ const displayPlantsDetails = (plants) => {
   plantsDetails.innerHTML += `
                 <div class="">
                     <h1 class="font-bold text-xl">${plants.name}</h1>
-                    <img class="h-50 w-full object-cover mt-4 mb-2" src="${plants.image}" alt="">
+                    <div class="w-full h-60 overflow-hidden rounded-xl mt-4 mb-2">
+                      <img class="h-full w-full object-cover duration-400 ease-in-out will-change-transform scale-100 hover:scale-110  duration-100 bg-center" src="${plants.image}" alt="">
+                    </div>
+                    
                     <h2><span class="font-bold ">Category:</span> ${plants.category}</h2>
                     <h2><span class="font-bold my-2">Price: ৳</span>${plants.price}</h2>
                     <p><span class="font-bold ">Description: </span>${plants.description}</p>
@@ -93,10 +96,14 @@ const displayTreeContainer = (trees) => {
   treeContainer.innerHTML = "";
   trees.forEach((tree) => {
     treeContainer.innerHTML += `
-    <div class="card bg-base-100  shadow-sm p-4">
+    <div class="bg-white p-4 h-auto">
       <figure>
-      <img class=" w-full h-[150px] object-cover bg-center " src="${tree.image}"
-      alt="Shoes" />
+      <div class="w-full h-[150px] overflow-hidden rounded-xl">
+        <img class=" w-full h-full duration-400 ease-in-out will-change-transform scale-100 hover:scale-110  duration-100 object-cover bg-center " src="${
+          tree.image
+        }"
+        alt="Shoes" />
+      </div>
       </figure>
       <div class=" mt-3">
         <h2 onclick="plantsData(${tree.id})" class="font-bold text-xl">${
@@ -112,7 +119,7 @@ const displayTreeContainer = (trees) => {
         <div class="card-actions justify-end">
             <button id='${
               tree.id
-            }' class="btn bg-[#15803D] text-white w-full rounded-full">Add to Card</button>
+            }' class="btn bg-[#15803D] text-white w-full rounded-full px-6 py-3 font-semibold transition-transform duration-300 hover:scale-110 hover:bg-green-500">Add to Card</button>
         </div>
       </div>
     </div>
@@ -157,7 +164,7 @@ const displayCard = (card) => {
   let priceCount = 0;
   card.forEach((item) => {
     cardContainer.innerHTML += `
-            <div class="p-3 bg-[#CFF0DC] rounded-lg text-xl mb-3 flex justify-between items-center"> 
+            <div class="p-3 bg-white shadow-lg rounded-lg text-xl mb-3 flex justify-between items-center"> 
               <div class="space-y-3">
                 <h3 id="cardTitle" class="font-bold">${item.title}</h3>
                 <p class="">৳ <span id="cardPrice">${item.price}</span> ✕ <span id="cardQuantity">${item.quantity}</span></p>
